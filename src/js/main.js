ec9f861db050f59b1b48a594ff835462335e507b
alext <softlink@bk.ru>
2021-09-24 23:29:27 +0700

    drag-n-drop

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,113 @@
-let wrap = document.querySelector('.wrap');
-let elems = document.querySelectorAll('.el');
-let btnPrev = document.querySelector('.prev');
-let btnNext = document.querySelector('.next');
+const wrap = document.querySelector('.wrap');
+const elems = document.querySelectorAll('.el');
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
 
 let totalCards = elems.length;
 
 let wrapStyles = getComputedStyle(wrap);
 
 let wrapWidth = wrap.offsetWidth;
-let counter = wrapStyles.getPropertyValue('--count');
-let visibleCards = wrapStyles.getPropertyValue('--visibleCards');
+let increment = Math.abs(wrapStyles.getPropertyValue('--count'));
+let visibleCards = Math.abs(wrapStyles.getPropertyValue('--visibleCards'));
 let fixedWidth = wrapStyles.getPropertyValue('--fixedWidth');
+let cardWidth = wrapStyles.getPropertyValue('--cardWidth');
 let template = wrapStyles.getPropertyValue('--template');
 
-// const properties {
-//     wrapWidth = wrap.offsetWidth,
-//     counter = wrapStyles.getPropertyValue('--count'), 
-//     visibleCards = wrapStyles.getPropertyValue('--visibleCards'),
-//     visibleCards = wrapStyles.getPropertyValue('--visibleCards'),
-//     fixedWidth = wrapStyles.getPropertyValue('--fixedWidth'),
-//     template = wrapStyles.getPropertyValue('--template'),
-// }
-
 function setProperties() { 
-    if (Number(visibleCards) > 0) {
-        wrap.style.setProperty('--template','var(--templateVisibleCards)');
-        wrap.style.setProperty('--visibleCards', visibleCards);
-        wrap.style.setProperty('--cardWidth', 'var(--visibleCardWidth)');
-    } 
-    else {
-        wrap.style.setProperty('--template', 'var(--templateFixedWidthCards)');
-        wrap.style.setProperty('--cardWidth', fixedWidth+'px');
-    }
+  if (visibleCards > 0) {
+    wrap.style.setProperty('--template','var(--templateVisibleCards)');
+    wrap.style.setProperty('--visibleCards', visibleCards);
+    wrap.style.setProperty('--cardWidth', 'var(--visibleCardWidth)');
+  } 
+  else {
+    wrap.style.setProperty('--template', 'var(--templateFixedWidthCards)');
+    wrap.style.setProperty('--cardWidth', fixedWidth+'px');
+  }
 
-    wrap.style.setProperty("--wrapWidth", wrapWidth);
-    wrap.style.setProperty("--totalCards", elems.length);
+  wrap.style.setProperty("--wrapWidth", wrapWidth);
+  wrap.style.setProperty("--totalCards", elems.length);
+  setDisabledBtnPrev();
 }
 
 function btnClickHandler(event) {
-    let target = event.target;
-    target === btnNext ? counter =--counter : counter =++counter;
-    wrap.style.setProperty("--count", counter);
-    setDisabledBtnNext(counter);
-    setDisabledBtnPrev();
+  let target = event.target;
+  target === btnNext ? --increment : ++increment;
+  wrap.style.setProperty("--count", increment);
+  setDisabledBtnNext(increment);
+  setDisabledBtnPrev();
 }
 
 function setDisabledBtnNext(counter) {
-    Math.abs(counter) === totalCards - 1 
-    ? btnNext.setAttribute("disabled", "disabled") : btnNext.removeAttribute("disabled");
+  let maxSlide = totalCards - (visibleCards ? visibleCards : 2);
+  Math.abs(counter) === maxSlide 
+  ? btnNext.setAttribute("disabled", "disabled") : btnNext.removeAttribute("disabled");
 }
 
 function setDisabledBtnPrev() {
-    counter == 0 ? btnPrev.setAttribute("disabled", "disabled") : btnPrev.removeAttribute("disabled");
+  increment == 0 ? btnPrev.setAttribute("disabled", "disabled") : btnPrev.removeAttribute("disabled");
 }
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+wrap.addEventListener('mousedown', (e) => {
+  isDown = true;
+  wrap.classList.add('active');
+  startX = e.pageX - wrap.offsetLeft;
+  scrollLeft = wrap.scrollLeft;
+});
+wrap.addEventListener('mouseleave', () => {
+  isDown = false;
+  wrap.classList.remove('active');
+});
+wrap.addEventListener('mouseup', () => {
+  isDown = false;
+  wrap.classList.remove('active');
+  if (startX > cardWidth) increment =--increment;
+  if (startX < cardWidth) increment =++increment;
+});
+wrap.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - wrap.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  wrap.scrollLeft = scrollLeft - walk;
+
+  console.log(walk);
+});
+
+
+
+
+
+// elems.forEach(el => el.addEventListener('mousedown', function(event) {
+    
+//   let shift = event.clientX - el.getBoundingClientRect().left;
+  
+//   document.addEventListener('mousemove', move);
+//   document.addEventListener('mouseup', up);
+  
+//   function move(event) {
+//     let left = event.clientX - el.getBoundingClientRect().left - shift;
+    
+//     if (left < el.offsetWidth) increment =--increment;
+//     if (left > el.offsetWidth) increment=++increment;
+    
+//     el.style.transform = 'translateX(' + left + 'px)';
+//   }
+    
+//   function up() {
+//     document.removeEventListener('mousemove', move);
+//     document.removeEventListener('mouseup', up);
+//   }
+// }))
+
+
+
+
+
 
 btnPrev.addEventListener('click', btnClickHandler);
 btnNext.addEventListener('click', btnClickHandler);
